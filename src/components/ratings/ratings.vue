0b6612f82246d40158c1aaf9bbe8c94b6dc2ab28
leon.Rong <347966863@qq.com>
2017-05-16 22:49:09 +0800

    评论组件顶部右半部分

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -8,18 +8,42 @@
 					<div class="rank">高于周边商家{{seller.rankRate}}%</div>
 				</div>
 				<div class="overview-right">
-					
+					<div class="score-wrapper">
+						<span class="title">
+							 服务态度
+						</span>
+						<star :size="36" :score="seller.serviceScore"></star>
+						<span class="score">{{seller.serviceScore}}</span>
+					</div>
+					<div class="score-wrapper">
+						<span class="title">
+							 商品评分
+						</span>
+						<star :size="36" :score="seller.foodScore"></star>
+						<span class="score">{{seller.foodScore}}</span>
+					</div>
+					<div class="delivery-wrapper">
+						<span class="title">
+							送达时间
+						</span>
+						<span class="delivery">{{seller.deliveryTime}}分钟</span>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 <script type="text/ecmascript-6">
+	import star from '@/components/star/star'
+
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
+		},
+		components: {
+			star
 		}
 	}
 </script>
@@ -63,5 +87,38 @@
 		font-size: 10px;
 		line-height: 10px;
 		color: rgb(147,153,159);
+	}
+	.score-wrapper, .delivery-wrapper {
+		line-height: 18px;
+		margin-bottom: 8px;
+		font-size: 0;
+	}
+	.score-wrapper .title {
+		 font-size: 12px;
+		 line-height: 18px;
+		 color: rgb(7,17,27);
+	}
+	.score-wrapper .star {
+		display: inline-block;
+		vertical-align: top;
+		margin-left: 12px;
+		margin-right: 4px;
+	}
+	.score-wrapper .score {
+		font-size: 12px;
+		vertical-align: top;
+		line-height: 18px;
+		color: rgb(255,153,0);
+	}
+	.delivery-wrapper .title {
+		font-size: 12px;
+		line-height: 18px;
+		color: rgb(7,17,27);
+		margin-right: 12px;
+	}
+	.delivery-wrapper .delivery {
+		font-size: 12px;
+		color: rgb(147,153,159);
+		line-height: 18px;
 	}
 </style>
